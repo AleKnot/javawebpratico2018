ee13fd767d18404123ad025e8e08486bb66b7840
Alex Rospide <alexrospide@gmail.com>
2018-03-11 18:46:04 -0300

    Json listando pessoas

--- pessoa/src/main/java/br/com/pessoa/dao/PessoaDAO.java
+++ pessoa/src/main/java/br/com/pessoa/dao/PessoaDAO.java
@@ -2,7 +2,6 @@ package br.com.pessoa.dao;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import br.com.pessoa.domain.Pessoa;
@@ -11,12 +10,12 @@ public class PessoaDAO {
 	
 	public List<Pessoa> findAll(){
 		
-		List<Pessoa> pessoas = new ArrayList();
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
 		
-		pessoas.add(new Pessoa("Mac Gayver", LocalDate.of(22, 12, 1987), 12365488, "https://image.freepik.com/free-icon/business-person-silhouette-wearing-tie_318-49988.jpg"));
-		pessoas.add(new Pessoa("Rambo", LocalDate.of(22, 12, 1987), 12365488, "https://image.freepik.com/free-icon/business-person-silhouette-wearing-tie_318-49988.jpg"));
-		pessoas.add(new Pessoa("Chuck Norris", LocalDate.of(22, 12, 1987), 12365488, "https://image.freepik.com/free-icon/business-person-silhouette-wearing-tie_318-49988.jpg"));
-		pessoas.add(new Pessoa("Bruce Lee", LocalDate.of(22, 12, 1987), 12365488, "https://image.freepik.com/free-icon/business-person-silhouette-wearing-tie_318-49988.jpg"));
+		pessoas.add(new Pessoa("Mac Gayver", LocalDate.of(2008, 5, 1), 12365488, "https://image.freepik.com/free-icon/business-person-silhouette-wearing-tie_318-49988.jpg"));
+		pessoas.add(new Pessoa("Rambo", LocalDate.of(2008, 5, 1), 12365488, "https://image.freepik.com/free-icon/business-person-silhouette-wearing-tie_318-49988.jpg"));
+		pessoas.add(new Pessoa("Chuck Norris", LocalDate.of(2008, 5, 1), 12365488, "https://image.freepik.com/free-icon/business-person-silhouette-wearing-tie_318-49988.jpg"));
+		pessoas.add(new Pessoa("Bruce Lee", LocalDate.of(2008, 5, 1), 12365488, "https://image.freepik.com/free-icon/business-person-silhouette-wearing-tie_318-49988.jpg"));
 		
 		
 		return pessoas;
