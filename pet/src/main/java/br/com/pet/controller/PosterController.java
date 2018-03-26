03b7083fe3f577c667b72f0c6c6b27e59fb184ca
AleKnot <alexrospide@gmail.com>
2018-03-26 15:51:40 -0300

    Improved fields on view form.html

--- pet/src/main/java/br/com/pet/controller/PosterController.java
+++ pet/src/main/java/br/com/pet/controller/PosterController.java
@@ -1,18 +1,25 @@
 package br.com.pet.controller;
 
 import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @MultipartConfig
-@WebServlet("/poster")
 public class PosterController {
 	
-	@GetMapping("/savePoster")
-	public void savePoster() {
-		
-		
+// http://www.pablocantero.com/blog/2010/09/29/upload-com-spring-mvc/
+	
+	
+	@RequestMapping(value = "upload", method = RequestMethod.POST)
+	public String upload(HttpServletRequest request) {
+	  MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+	  MultipartFile multipartFile = multipartRequest.getFile("file");
+	  return "redirect:upload-success";
 	}
 
 }
